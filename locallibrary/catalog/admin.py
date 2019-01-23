be30c70c25195f3e2adba4a030410e77cf22c1fe
tengdann <tengdann@umich.edu>
2019-01-23 02:06:26 +0000

    Admin tutorial complete

--- locallibrary/catalog/admin.py
+++ locallibrary/catalog/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from catalog.models import Author, Genre, Book, BookInstance
 
 # Register your models here.
+
+# admin.site.register(Book)
+# admin.site.register(Author)
+admin.site.register(Genre)
+# admin.site.register(BookInstance)
+
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
+
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'display_genre')
+
+class BookInstanceAdmin(admin.ModelAdmin):
+    list_filter = ('status', 'due_back')
+
+admin.site.register(Book, BookAdmin)
+admin.site.register(Author, AuthorAdmin)
+admin.site.register(BookInstance, BookInstanceAdmin)
