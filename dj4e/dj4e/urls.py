df01ddeedb3ee2c6a62fc00f7f3ad98cdb79fecd
Danny Teng <tengdann@umich.edu>
2019-02-19 18:18:43 -0500

    Local not working

--- dj4e/dj4e/urls.py
+++ dj4e/dj4e/urls.py
@@ -18,8 +18,5 @@ from django.urls import path
 from django.urls import include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('', include('home.urls')),
-    path('accounts/', include('django.contrib.auth.urls')),
-    path('autos/', include('auto.urls')),
 ]
