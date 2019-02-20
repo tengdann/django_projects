0cdb3603b2fab492636dbe76d0bf8b55884c21cb
Danny Teng <tengdann@umich.edu>
2019-02-19 19:11:52 -0500

    ...

--- dj4e/dj4e/urls.py
+++ dj4e/dj4e/urls.py
@@ -19,4 +19,5 @@ from django.conf.urls import url, include
 urlpatterns = [
     url(r'', include('home.urls')),
     url(r'admin/', admin.site.urls),
+    url(r'accounts/', include('django.contrib.auth.urls')),
 ]
