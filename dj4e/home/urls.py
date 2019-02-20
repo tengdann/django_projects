78b7e25ad20a3dfbdb76ef3dcfdb59c8c78c6ac5
tengdann <tengdann@umich.edu>
2019-02-19 22:05:46 -0500

    FUCKING DJANGO

--- dj4e/home/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import url
-from . import views
-from django.views.generic import TemplateView
-
-# https://docs.djangoproject.com/en/2.1/topics/http/urls/
-urlpatterns = [
-    url(r'', views.index, name = 'index'),  
-]
