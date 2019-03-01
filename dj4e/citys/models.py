21b6feabace71a060c355b24901a619dec916123
Danny Teng <tengdann@umich.edu>
2019-03-01 13:07:03 -0500

    citys/models.py updated; need to migrate

--- dj4e/citys/models.py
+++ dj4e/citys/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.core.validators import MinLengthValidator
 
 # Create your models here.
+class State(models.Model):
+    name = models.CharField(
+            max_length = 200,
+            validators = [MinLengthValidator(2, "State must be greater than 1 character")]
+    )
+
+    def __str__(self):
+        return self.name
+
+class City(models.Model):
+    nickname = models.CharField(
+            max_length = 200,
+            validators = [MinLengthValidator(2, "Nickname must be greater than 1 character")]
+    )
+    population = models.PositiveIntegerField()
+    slogan = models.CharField(max_length=300)
+    state = models.ForeignKey('State', on_delete=models.CASCADE, null=False)
+
+    def __str__(self):
+        return self.nickname
