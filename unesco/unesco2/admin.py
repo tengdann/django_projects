8cf072740244e3a2ea0b8af4c7ad9d7089895a5c
tengdann <tengdann@umich.edu>
2019-03-21 22:35:48 +0000

    unesco commit

--- unesco/unesco2/admin.py
+++ unesco/unesco2/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from unesco2.models import Site, Category, State, Region, Iso
 
 # Register your models here.
+
+admin.site.register(Category)
+admin.site.register(State)
+admin.site.register(Region)
+admin.site.register(Iso)
+admin.site.register(Site)
--- unesco/unesco2/views.py
+++ unesco/unesco2/views.py
@@ -1,3 +1,37 @@
 from django.shortcuts import render
+from django.views import generic
+from django.shortcuts import render
+
+from unesco2.models import Category, State, Region, Iso, Site
 
 # Create your views here.
+
+class CategoryListView(generic.ListView):
+    model = Category
+
+class CategoryDetailView(generic.DetailView):
+    model = Category
+
+class StateListView(generic.ListView):
+    model = State
+
+class StateDetailView(generic.DetailView):
+    model = State
+
+class RegionListView(generic.ListView):
+    model = Region
+
+class RegionDetailView(generic.DetailView):
+    model = Region
+
+class IsoListView(generic.ListView):
+    model = Iso
+
+class IsoDetailView(generic.DetailView):
+    model = Iso
+
+class SiteListView(generic.ListView):
+    model = Site
+
+class SiteDetailView(generic.DetailView):
+    model = Site
