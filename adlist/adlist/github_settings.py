7bc572f80d0e67942686e0f98c360bc62b267a83
tengdann <tengdann@umich.edu>
2019-04-02 19:07:52 +0000

    stuff

---
+++ adlist/adlist/github_settings.py
@@ -0,0 +1,16 @@
+# Copy this file to github_settings.py (don't check it into github)
+
+# Go to https://github.com/settings/developers
+
+# Add a New OAuth2 App - GIve it a name
+
+# If you are running on localhost, make the callback url be
+# http://127.0.0.1:8000/oauth/complete/github/
+
+# If you are on the real internet (or using ngrok) make the callback url be
+# https://samples.dj4e.com/oauth/complete/github/
+
+# Then copy the client_key and secret to this file
+
+SOCIAL_AUTH_GITHUB_KEY = '51a6b2dbaea0c7f4c75e'
+SOCIAL_AUTH_GITHUB_SECRET = '2ae4f86a16e460e98531abc9362efb2d84c61479'
